6c8d6c7ce0cc8438eacee5429123845f69813613
IvanKot <wocker@i.ua>
2023-06-02 11:56:55 +0300

    Update index.php

--- app/views/gallery/index.php
+++ app/views/gallery/index.php
@@ -1,6 +1,15 @@
 <main class="main">
     <div class="wrapper_gallery">
         <div class="gallery">
+            <?php
+            $i = 0;
+            foreach ($photoNames as $photoSrc => $name):
+                $i++; ?>
+                <figure>
+                    <img title='<?= $name ?>' src='<?= $photoSrc ?>'>
+                    <figcaption><?= $name ?></figcaption>
+                </figure>
+            <?php endforeach; ?>
         </div>
     </div>
 </main>
@@ -9,12 +18,3 @@
     <img class="overlay__photo" src="http://<?= $_SERVER["SERVER_NAME"]; ?>/public/img/gallery/(4).jpg">
     <div class="overlay__arrow overlay__arrow--right"></div>
 </div>
-<?php
-$i = 0;
-foreach ($photoNames as $photoSrc => $name):
-    $i++; ?>
-    <figure>
-        <img class='foto_img' data-img-id=<?= $i - 1 ?> title='<?= $name ?>' src='<?= $photoSrc ?>'>
-        <figcaption><?= $name ?></figcaption>
-    </figure>
-<?php endforeach; ?>
