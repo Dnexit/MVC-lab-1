81265e156ae20a5e2579a6dae8742d668d06a948
IvanKot <wocker@i.ua>
2023-06-02 12:09:52 +0300

    2-е задание

--- app/controllers/InterestsController.php
+++ app/controllers/InterestsController.php
@@ -6,10 +6,16 @@ use app\core\Controller;
 
 class InterestsController extends Controller
 {
+    private $content = [];
+    private $interests = [];
 
     public function indexAction(){
-        $vars = [
+        $this->content = $this->model->getContent();
+        $this->interests = $this->model->getInterests();
 
+        $vars = [
+            "content" => $this->content,
+            "interests" => $this->interests
         ];
 
         $this->view->render("Интересы", $vars);
